1ae3c4391673766050c3a40c2b6ca3dea4c6b282
David Peach <mail@davidpeach.co.uk>
2024-05-02 07:47:15 +0100

    debugging ap

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 Route::get('/.well-known/webfinger', function () {
     return [
-        'subject' => 'acct:david@feature-activitypub.davidpeach.me',
+        'subject' => 'acct:david@ap1.davidpeach.me',
         'links' => [
             [
                 'rel' => 'self',
                 'type' => 'application/activity+json',
-                'href' => 'https://feature-activitypub.davidpeach.me/david',
+                'href' => 'https://ap1.davidpeach.me/david',
             ],
         ],
     ];
@@ -21,31 +21,31 @@ Route::get('/david', function () {
             'https://w3id.org/security/v1',
         ],
         'type' => 'Person',
-        'id' => 'https://feature-activitypub.davidpeach.me/david',
-        'following' => 'https://feature-activitypub.davidpeach.me/david/following',
-        'followers' => 'https://feature-activitypub.davidpeach.me/david/followers',
-        'inbox' => 'https://feature-activitypub.davidpeach.me/david/inbox',
-        'outbox' => 'https://feature-activitypub.davidpeach.me/david/outbox',
-        'featured' => 'https://feature-activitypub.davidpeach.me/david/collections/featured',
-        'featuredTags' => 'https://feature-activitypub.davidpeach.me/david/collections/tags',
+        'id' => 'https://ap1.davidpeach.me/david',
+        'following' => 'https://ap1.davidpeach.me/david/following',
+        'followers' => 'https://ap1.davidpeach.me/david/followers',
+        'inbox' => 'https://ap1.davidpeach.me/david/inbox',
+        'outbox' => 'https://ap1.davidpeach.me/david/outbox',
+        'featured' => 'https://ap1.davidpeach.me/david/collections/featured',
+        'featuredTags' => 'https://ap1.davidpeach.me/david/collections/tags',
         'endpoints' => [
-            'sharedInbox' => 'https://feature-activitypub.davidpeach.me/inbox',
+            'sharedInbox' => 'https://ap1.davidpeach.me/inbox',
         ],
         'name' => 'David Peach',
         'summary' => "<p>A bio goes here</p>\n",
-        'url' => 'https://feature-activitypub.davidpeach.me/david',
+        'url' => 'https://ap1.davidpeach.me/david',
         'manuallyApprovesFollowers' => false,
         'discoverable' => true,
 
         'preferredUsername' => 'david',
         'published' => '2020-01-01T00:00:00Z',
 
-        // 'inbox' => 'https://feature-activitypub.davidpeach.me/inbox',
-        // 'outbox' => 'https://feature-activitypub.davidpeach.me/outbox',
+        // 'inbox' => 'https://ap1.davidpeach.me/inbox',
+        // 'outbox' => 'https://ap1.davidpeach.me/outbox',
 
         'publicKey' => [
-            'id' => 'https://feature-activitypub.davidpeach.me/david#main-key',
-            'owner' => 'https://feature-activitypub.davidpeach.me/david',
+            'id' => 'https://ap1.davidpeach.me/david#main-key',
+            'owner' => 'https://ap1.davidpeach.me/david',
             'publicKeyPem' => "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxKW+hzgm/knxT7DHI0sL\nzfdnZiKX2WXRBRwEJCdQ7p6B4QuA5JB5MqVTnT8WA4S3m0NMuNsCBHV7kNcFW1iF\ntuVqj/xZeDiwVg9SYwQs79hfC0OPiJnS5a/YXEqaNQg6u/os5K3O0+9FiYCvi3lf\nb8bb05yoNh0bEL7MM2OuZVaLVkUkf2wugOT1it+NmBzdAMb6CUG1WkLJllnFDyoq\nEUmoQmIdUGZ83GlD9CQCv7Ne3IjXqxOw/CPmtUaDM4naauGKGeD0MHoAb++aFrig\nThOURn7JaAsIxd3dMbLibbCmSfpnMwCRNG7X4uZrkqxPNIjM+EIcgcNRxng9+JWP\nYwIDAQAB\n-----END PUBLIC KEY-----",
         ],
     ];
